08efb9fd3d02a4bbcf315d7185ce05a54264d007
Sziyi <3275157006@qq.com>
2022-07-06 20:06:19 +0800

    feat(axios api): 跨域

---
+++ src/api/user.js
@@ -0,0 +1,31 @@
+import request from '../utils/request'
+
+// 获取验证码
+const getCaptcha = () => {
+  return request({ url: '/captcha', method: 'GET' })
+}
+
+// 登录接口
+const login = (data) => {
+  return request({
+    url: `/login?username${data.username}&password=${data.password}&code=${data.code}&token=${data.token}`,
+    method: 'POST'
+  })
+}
+
+// 获取用户信息接口
+const getUserInfo = () => {
+  return request({ url: '/sys/userInfo', method: 'GET' })
+}
+
+// 退出登录接口
+const logout = () => {
+  return request({ url: '/logout', method: 'POST' })
+}
+
+export default {
+  getCaptcha,
+  login,
+  getUserInfo,
+  logout
+}
---
+++ src/utils/request.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import store from '../store'
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  (config) => {
+    // 将token通过请求发送给后台
+    const token = store.getters.token
+    if (token) config.headers.Authorization = token
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+// 统一路由传参
+const request = (options) => {
+  if (options.method.toLowerCase() === 'get') {
+    options.params = options.data || {}
+  }
+  return service(options)
+}
+
+export default request
